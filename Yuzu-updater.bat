52010e448b28107c65ae07120dffca609ad19692
nondetect <nondetect@users.noreply.github.com>
2023-06-17 20:05:18 +0200

    Update Yuzu-updater v0.0.3

--- Yuzu-updater.bat
+++ Yuzu-updater.bat
@@ -1,5 +1,5 @@
 @echo off
-set "scriptver=0.0.2"
+set "scriptver=0.0.3"
 title Yuzu-Updater %scriptver%
 set dlfile=%temp%\yuzu.zip
 set dirmain=yuzu
@@ -11,6 +11,7 @@ set linkpine=pineappleEA/pineapple-src
 set shaderp=user\shader
 set shadera=%AppData%\yuzu\shader
 set "cc=_"
+set "it=standard"
 
 goto :START
 
@@ -43,20 +44,22 @@ goto :CHECK_VER
 :CHECK_VER
 set link=https://api.github.com/repos/%br%/releases/latest
 set yuzulastverpath=.\%diryuzu%\ver.txt
-IF NOT EXIST %yuzulastverpath% (set oldv=none)
+IF NOT EXIST .\%diryuzu%\ver.txt (set oldv=none)
 set /p oldv=<%yuzulastverpath%
 for /f "tokens=2 delims=, " %%a in ('curl -s %link% ^| findstr /L "tag_name"' ) do ( set v=%%a )
 cls
 echo.
-echo          **You choose - %diryuzu%**
+echo                    **You choose - %diryuzu%** [%it%]
+echo     (press "4" to toggle between standard and portable installation)     
 echo.
 echo     Current online %diryuzu% version - %v%
 echo     Current file version - %oldv%
 echo.
-echo     [%cc%] - Clean Shader Folder (press "4" to toggle activate/deactivate)
+echo     [%cc%] - Clean Shader Folder (press "5" to toggle activate/deactivate)
 echo.
-choice /C:1234 /N /M "   Do you want update %diryuzu% to ver - %v%? [1 - Yes, 2 - No, 3 - Exit]: "
-if errorlevel 4 goto:SWITCH
+choice /C:12345 /N /M " Do you want update %diryuzu% to ver - %v%? [1 - Yes, 2 - No, 3 - Exit]: "
+if errorlevel 5 goto:SWITCH
+if errorlevel 4 goto:PORTABLE
 if errorlevel 3 goto exit
 if errorlevel 2 goto:START
 if errorlevel 1 goto:DOWNLOAD
@@ -71,15 +74,19 @@ Xcopy .\%defdir%\ .\%diryuzu% /E /H /C /I /Y
 del /f /q %dlfile%
 rmdir /s /q .\%defdir%
 echo %v% > .\%diryuzu%\ver.txt
+if %it%==portable (
+    if not exist .\%diryuzu%\user mkdir .\%diryuzu%\user
+)
 if %cc%==x call :SHADERCLEAN
 set "cc=_"
+set "it=standard"
 goto :START
 
 :SHADERCLEAN
-if not exist .\%diryuzu%\%shaderp% (
-    rmdir /s /q %shadera%
-    ) else (
+if %it%==portable (
     rmdir /s /q .\%diryuzu%\%shaderp%
+    ) else (
+    rmdir /s /q %shadera%    
 )
 goto :EOF
 
@@ -88,5 +95,13 @@ if %cc% ==_ (
         set "cc=x"
     ) else (
         set "cc=_"
+    )
+goto :CHECK_VER
+
+:PORTABLE
+if %it% ==standard (
+        set "it=portable"
+    ) else (
+        set "it=standard"
     )
 goto :CHECK_VER
